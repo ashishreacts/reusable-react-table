d2b8f03e5ff9bce059e6d2af151a552fb81e3658
AJ5154 <ashishjadhav597@gmail.com>
2024-02-06 12:29:05 +0530

    resolve import error

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -1,9 +1,12 @@
+import exp from "constants";
 import React, { FC } from "react";
 
 type TableProps = {
   data: any[];
 };
 
-export const Table: FC<TableProps> = (props: TableProps) => {
+const Table: FC<TableProps> = (props: TableProps) => {
   return <div>index</div>;
 };
+
+export default Table;
