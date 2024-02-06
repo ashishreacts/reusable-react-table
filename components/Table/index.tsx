a8d77cfd5dda8eabe78f64c93d3815133e49642a
AJ5154 <ashishjadhav597@gmail.com>
2024-02-06 16:34:33 +0530

    table is created

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -1,3 +1,13 @@
+import {
+  Paper,
+  TableContainer,
+  Table as MUITable,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+} from "@mui/material";
 import { ColumnDef } from "@tanstack/react-table";
 import React, { Dispatch, FC, SetStateAction } from "react";
 
@@ -13,8 +23,45 @@ type TableProps = {
   searchLabel: string;
 };
 
+const columns = [
+  { id: "name", label: "Name" },
+  { id: "email", label: "Email" },
+  { id: "gender", label: "Gender" },
+  { id: "status", label: "Status" },
+];
+
+const rows = [
+  { name: "Ashish", email: "aj@gmail.com", gender: "Male", status: "active" },
+  { name: "Ashish", email: "aj@gmail.com", gender: "Male", status: "active" },
+  { name: "Ashish", email: "aj@gmail.com", gender: "Male", status: "active" },
+  { name: "Ashish", email: "aj@gmail.com", gender: "Male", status: "active" },
+];
 const Table: FC<TableProps> = (props: TableProps) => {
-  return <div>index</div>;
+  return (
+    <TableContainer component={Paper}>
+      <MUITable>
+        <TableHead>
+          <Typography component="h1" variant="h4">
+            User Table
+          </Typography>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column.id}>{column.label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow key={index}>
+              {columns.map((column) => (
+                <TableCell key={column.id}>{row[column.id]}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </MUITable>
+    </TableContainer>
+  );
 };
 
 export default Table;
